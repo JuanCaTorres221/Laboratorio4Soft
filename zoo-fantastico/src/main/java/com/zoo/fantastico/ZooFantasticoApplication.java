8be262dcd9c42fe0b532b374f5ce677d7dc1046f
JuanCaTorres221 <geforcenecesito@gmail.com>
2025-10-24 12:09:00 -0500

    corrección application.java

--- zoo-fantastico/src/main/java/com/zoo/fantastico/ZooFantasticoApplication.java
+++ zoo-fantastico/src/main/java/com/zoo/fantastico/ZooFantasticoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ZooFantasticoApplication {
-    //prueba
+
 	public static void main(String[] args) {
 		SpringApplication.run(ZooFantasticoApplication.class, args);
 	}
